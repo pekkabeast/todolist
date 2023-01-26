5b9f2558605b587af97ecfbdc50ba3d1f3962e65
Victor Chan <vctrchn@hotmail.ca>
2023-01-25 21:13:49 -0500

    added content to sidepane and some styling

---
+++ src/modules/display.js
@@ -0,0 +1,7 @@
+//Display for the sidePane
+
+//clicking today should show todays to do list
+const todayBtn = document.getElementById("today");
+todayBtn.addEventListener("click", showTodayList);
+//clicking upcoming should show upcoming to do list
+const upcomingBtn = document.getElementById("upcoming");
