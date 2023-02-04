b744cc0d4d6b0d14dc4adc0b597f5b666f43762e
Victor Chan <vctrchn@hotmail.ca>
2023-02-04 18:17:56 -0500

    adjusting styles for project buttons

--- src/modules/display.js
+++ src/modules/display.js
@@ -196,7 +196,7 @@ const display = (() => {
       deleteProjectBtn.classList.add("delete-tile-btn");
       deleteProjectBtn.classList.add("btn");
       deleteProjectBtn.innerHTML =
-        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>delete</title><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" /></svg>';
+        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill = "white"><title>delete</title><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" /></svg>';
       tile.appendChild(deleteProjectBtn);
       tile.id = tileText;
       return tile;
@@ -223,12 +223,12 @@ const display = (() => {
       const dropdownProjectsBtn = document.getElementById("showProjects");
       if (document.getElementById("project-btn-wrapper") == null) {
         dropdownProjectsBtn.innerHTML =
-          '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Show Projects</title><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>';
+          '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Hide Projects</title><path d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" /></svg>';
         dropdownProjectsBtn.removeEventListener("click", hideProjects);
         dropdownProjectsBtn.addEventListener("click", showProjects);
       } else {
         dropdownProjectsBtn.innerHTML =
-          '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Hide Projects</title><path d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" /></svg>';
+          '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Show Projects</title><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>';
         dropdownProjectsBtn.removeEventListener("click", showProjects);
         dropdownProjectsBtn.addEventListener("click", hideProjects);
       }
@@ -264,6 +264,12 @@ const display = (() => {
       generateProjectNavContent,
       initProjectBtns,
     };
+  })();
+
+  const mainContentDom = (() => {
+    const showProjectContent = () => {};
+    const showTodayContent = () => {};
+    const showUpcomingContent = () => {};
   })();
 
   return {
